35f3868ab7d5d9ef24d8793c2649bfb175d0a035
Thiesa <thiesa.c@gmail.com>
2023-08-26 17:33:26 -0400

    requisito 4 sem checar se user existe

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -18,17 +18,17 @@ const createUser = async (req, res) => {
     const user = await UserService.createUser({ displayName, email, password, image });
   //   const { token } = await login(req, res);
   // console.log(`token: ${token}`);
-  const userExists = await UserService.getByEmail(email);
+  // const userExists = await UserService.getByEmail(email);
   const token = generateToken(user);
 
   if (user && user.status === 400) {
     return res.status(400).json({ message: user.data.message });
   }
-  if (!userExists) {
-    res.status(201).json({ token });
-  } else if (userExists.isNewRecord === false) {
-    return res.status(409).json({ message: 'User already registered' });
-  }
+  // if (!userExists) {
+  //   res.status(201).json({ token });
+  // } else if (userExists.isNewRecord === false) {
+  //   return res.status(409).json({ message: 'User already registered' });
+  // }
 
   res.status(201).json({ token });
 };
