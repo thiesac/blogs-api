1f8647682c039bc0fdcf9722bc652924b9ee3b86
Thiesa <thiesa.c@gmail.com>
2023-08-26 10:52:24 -0400

    fix: complexidade requisito 3

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -3,25 +3,41 @@ const { LoginService } = require('../services');
 
 const secret = process.env.JWT_SECRET;
 
+const validateLoginFields = (email, password) => {
+  if (!email || !password) {
+    throw new Error('Some required fields are missing');
+  }
+};
+
+const validateUserCredentials = (user, password) => {
+  if (!user || user.password !== password) {
+    throw new Error('Invalid fields');
+  }
+};
+
+const generateToken = (user) => {
+  const jwtConfig = {
+    expiresIn: '7d',
+    algorithm: 'HS256',
+  };
+
+  return jwt.sign({ data: { userId: user.id } }, secret, jwtConfig);
+};
+
 module.exports = async (req, res) => {
+  try {
     const { email, password } = req.body;
 
-    if (!email || !password) {
-      return res.status(400).json({ message: 'Some required fields are missing' });
-    }
+    validateLoginFields(email, password);
 
     const user = await LoginService.getByEmail(email);
-    if (!user || user.password !== password) {
-      return res.status(400).json({ message: 'Invalid fields' });
-    }
 
-    const jwtConfig = {
-      expiresIn: '7d',
-      algorithm: 'HS256',
-    };
+    validateUserCredentials(user, password);
 
-    const token = jwt.sign({ data: { userId: user.id } }, secret, jwtConfig);
+    const token = generateToken(user);
+
     res.status(200).json({ token });
-
-    return res.status(400).json({ message: 'Internal error' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
