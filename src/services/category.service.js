a552758acf221d4e63bc22bc0405bd959f633ad3
Thiesa <thiesa.c@gmail.com>
2023-08-27 11:32:10 -0400

    chore: removendo console.log

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -3,7 +3,6 @@ const { categorySchema } = require('./validations/schemas');
 
 const createCategory = async ({ name }) => {
   const validationResult = categorySchema.validate({ name });
-  console.log(validationResult);
   if (validationResult.error) {
     console.log('deu erro');
     return { status: 400, data: { message: '"name" is required' } };
