6f9b35df73f305a738f43d7f5deccc14b05522ac
Thiesa <thiesa.c@gmail.com>
2023-08-28 16:35:01 -0400

    rota /post comentada

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -2,7 +2,7 @@ const route = require('express').Router();
 const { postController } = require('../controllers');
 const authMiddleware = require('../middlewares/auth.middleware');
 
-route.post('/', postController.createPost);
+// route.post('/', postController.createPost);
 route.get('/', authMiddleware, postController.getAll);
 
 module.exports = route;
